9cd3d67e90e3fdfb745994696363f0dcb074d893
Pedro Jose <pedro5_crevi@hotmail.com>
2022-10-09 21:00:57 +0200

    Ejemplo de componente y pruebas varias

---
+++ my-app/src/app/Fruta/fruta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fruta',
+  templateUrl: './fruta.component.html',
+  styleUrls: ['./fruta.component.css']
+})
+export class FrutaComponent {
+    public resumen = "Esto es el componente resumen"
+    public pagina = 1
+}
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrutaComponent } from './Fruta/fruta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrutaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, FrutaComponent]
 })
 export class AppModule { }
